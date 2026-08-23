1aa9fe107a45c5b9a1f2a3b62c0dfe9f00a83774
Shahab Mirzareza <shahabmi@kth.se>
2026-08-23 22:11:21 +0200

    Improved server.py code according to PEP8

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""This module is used for web deployment."""
 from flask import Flask,request,render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,24 +6,26 @@ app=Flask(__name__)
 
 @app.route('/')
 def starter():
+    """ starter page"""
     return render_template('index.html')
 
 
 @app.route('/emotionDetector')
 def emotion_detector_func():
+    """ function to detect emotions in a sentence"""
     text_to_analyze=request.args.get('textToAnalyze')
     result=emotion_detector(text_to_analyze)
 
-    if result['dominant_emotion']==None:
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
 
     res= (
         f"For the given statement, the system response is "
         f"'anger': {result['anger']}, "
-        f"'disgust': {result['disgust']}, " 
-        f"'fear': {result['fear']}, " 
-        f"'joy': {result['joy']} and " 
-        f"'sadness': {result['sadness']}. " 
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
         f"The dominant emotion is <strong>{result['dominant_emotion']}</strong>.")
     return res
 
